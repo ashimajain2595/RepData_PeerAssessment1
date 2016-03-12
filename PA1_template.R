a0b6c80b62e74e1fa0746d1e3fb0fe70849fcdcb
Ashima Jain <ashimajain2595@gmail.com>
2016-03-12 22:01:01 +0530

    First commit

--- PA1_template.R
+++ PA1_template.R
@@ -1,18 +1,7 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
-
-
-## Loading and preprocessing the data
-
 unzip("activity.zip")
 data <- read.csv("activity.csv")
 
 
-## What is mean total number of steps taken per day?
 stepsperday <- tapply(data$steps, data$date, sum, na.rm=TRUE)
 hist(stepsperday, col="blue", breaks=50, xlab="Total steps per day", main="Histogram of total steps per day")
 
@@ -20,7 +9,6 @@ mean_spd <- mean(stepsperday, na.rm=TRUE)
 median_spd <- median(stepsperday, na.rm=TRUE)
 
 
-## What is the average daily activity pattern?
 meandata <- aggregate(data$steps, by=list(data$interval), mean, na.rm=TRUE)
 names(meandata) <- c("interval", "steps")
 plot(meandata, type="l", col="red", xlab="5-minute interval", ylab="average number of steps taken")
@@ -28,7 +16,6 @@ plot(meandata, type="l", col="red", xlab="5-minute interval", ylab="average numb
 maxsteps <- meandata[which.max(meandata$steps),]
 
 
-## Imputing missing values
 missing_val <- sum(is.na(data$steps))
 
 fill <- function(steps, interval) {
@@ -49,8 +36,6 @@ mean_tspd <- mean(totalstepsperday)
 median_tspd <- median(totalstepsperday)
 
 
-
-## Are there differences in activity patterns between weekdays and weekends?
 days <- function(date){
   day <- weekdays(date)
   if (day %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"))
@@ -63,4 +48,5 @@ nomiss_data$day <- sapply(nomiss_data$date, days)
 
 nomiss_mean <- aggregate(steps ~ interval + day, data = nomiss_data, mean)
 library(ggplot2)
-ggplot(nomiss_mean, aes(interval, steps)) + geom_line(colour="#000099") + facet_grid(day ~ .) + xlab("5-minute interval") + ylab("average number of steps taken")
+ggplot(nomiss_mean, aes(interval, steps)) + geom_line(colour="#000099") + facet_grid(day ~ .) + 
+  xlab("5-minute interval") + ylab("average number of steps taken")
